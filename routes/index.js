4842fe9f8745bc43a493000fe85d28b8ecbef0ea
Praful Mathur <prafmathur@gmail.com>
2014-02-12 22:56:20 -0500

    added twilio locally

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@
 
 var accountSid = 'ACf0cc48a7b6b9ec6c166dfe9fc7cdde6d';
 var authToken = "d210dfcb2c4a87d8502dbab01401f61b";
-var client = require('/usr/local/lib/node_modules/twilio')(accountSid, authToken);
+var client = require('../node_modules/twilio')(accountSid, authToken);
 
 
 exports.index = function(req, res){
